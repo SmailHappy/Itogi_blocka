2408b0e2acc479477b655640f48d3ad59a22aa9d
Alexey <alex-ey98@mail.ru>
2022-11-11 15:10:03 +0300

    Создание и заполнение массива

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Из N элементов массива A формируется новый массив размер элементов которого меньше либо равна 3");
+System.Console.Write("Введите количество N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[num];
+for (int i = 0; i < num; i++) {
+    System.Console.Write($"Введите {i + 1} элемент массива A: ");
+    array[i] = Console.ReadLine();
+}
